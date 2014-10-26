bd17996a11c7171b99b2198cee3d025086597d13
britech <brian.angyang@gmail.com>
2014-10-26 14:31:30 +0800

    New Feature
    - Added directory of departments
    - Inserted filtering feature to department listing

--- protected/controllers/DepartmentController.php
+++ protected/controllers/DepartmentController.php
@@ -2,9 +2,11 @@
 
 namespace org\csflu\isms\controllers;
 
-use org\csflu\isms\core\Controller as Controller;
+use org\csflu\isms\core\Controller;
 use org\csflu\isms\exceptions\ValidationException as ValidationException;
 use org\csflu\isms\service\commons\DepartmentServiceSimpleImpl as DepartmentService;
+use org\csflu\isms\core\ApplicationConstants;
+use org\csflu\isms\util\ApplicationUtils;
 /**
  * Description of DepartmentController
  *
@@ -16,6 +18,7 @@ class DepartmentController extends Controller{
     
     public function __construct() {
         $this->checkAuthorization();
+        $this->layout = 'column-2';
         $this->departmentService = new DepartmentService();
     }
     
@@ -34,4 +37,33 @@ class DepartmentController extends Controller{
         }
     }
     
+    public function index(){
+        $this->title = ApplicationConstants::APP_NAME.' - Manage Departments';
+        $this->render('department/index', array(
+            'breadcrumb'=>array(
+                'Home'=>array('site/index'),
+                'Manage Departments'=>'active'),
+            'sidebar'=>array(
+                'data'=>array(
+                    'header'=>'Actions',
+                    'links'=>array(
+                        'Add New Department' => array('role/createDepartment')
+                    )
+                )
+            )
+        ));
+    }
+    
+    public function listDepartments(){
+        $departments = $this->departmentService->listDepartments();
+        $data = array();
+        foreach($departments as $department) {
+            array_push($data, array(
+                'name'=>$department->code.' - '.$department->name,
+                'action'=>ApplicationUtils::generateLink(
+                        array('department/updateDepartment', 'id'=>$department->id), 
+                        'Update Department')));
+        }
+        $this->renderAjaxJsonResponse($data);
+    }
 }
---
+++ protected/views/department/index.php
@@ -0,0 +1,8 @@
+<?php 
+namespace org\csflu\isms\views;
+
+use org\csflu\isms\util\FormGenerator as Form;
+?>
+<script type="text/javascript" src="protected/js/department/index.js"></script>
+<div id="departmentList"></div>
+
