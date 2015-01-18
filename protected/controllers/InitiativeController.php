be5c0d3e303fae41a32c2d6ce6b623e0ceb275fe
britech <brian.angyang@gmail.com>
2015-01-18 13:07:21 +0800

    Updated Fixtures
    - added controller for the initiative component
    - added an index page for management of initiatives

---
+++ protected/controllers/InitiativeController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace org\csflu\isms\controllers;
+
+use org\csflu\isms\core\Controller;
+use org\csflu\isms\core\ApplicationConstants;
+use org\csflu\isms\service\map\StrategyMapManagementServiceSimpleImpl as StrategyMapManagementService;
+
+/**
+ * Description of InitiativeController
+ *
+ * @author britech
+ */
+class InitiativeController extends Controller {
+
+    private $logger;
+    private $mapService;
+
+    public function __construct() {
+        $this->checkAuthorization();
+        $this->logger = new \Logger(__CLASS__);
+        $this->mapService = new StrategyMapManagementService();
+    }
+
+    public function index($map) {
+        $strategyMap = $this->loadMapModel($map);
+
+        $this->title = ApplicationConstants::APP_NAME . ' - Manage Initiatives';
+        $this->layout = 'column-2';
+        $this->render('initiative/index', array(
+            'breadcrumb' => array(
+                'Home' => array('site/index'),
+                'Strategy Map Directory' => array('map/index'),
+                'Strategy Map' => array('map/view', 'id' => $strategyMap->id),
+                'Manage Initiatives' => 'active'
+            ),
+            'sidebar' => array(
+                'data' => array(
+                    'header' => 'Actions',
+                    'links' => array(
+                        'Create an Initiative' => array('initiative/create', 'map' => $strategyMap->id)
+                    )
+                )
+            )
+        ));
+    }
+
+    private function loadMapModel($id) {
+        $map = $this->mapService->getStrategyMap($id);
+        if (is_null($map->id)) {
+            $this->setSessionData('notif', array('class' => '', 'message' => 'Strategy Map not found'));
+            $this->redirect(array('map/index'));
+        }
+        return $map;
+    }
+
+}
