9e55f5526a51b0c23c45c84c2e5c9299eed39f28
britech <brian.angyang@gmail.com>
2015-01-25 20:17:56 +0800

    Updated Fixture
    - updated property
    - added value-binding operation
    - added new instance checking operation
    - added form attribute names
    - added form validation

--- protected/models/initiative/Phase.php
+++ protected/models/initiative/Phase.php
@@ -9,29 +9,70 @@ use org\csflu\isms\models\initiative\Component;
  * Description of Phase
  *
  * @property String $id
+ * @property int $phaseNumber
  * @property String $title
  * @property String $description
- * @property int $positionOrder
  * @property Component[] $components
  * @author britech
  */
 class Phase extends Model {
 
     private $id;
+    private $phaseNumber;
     private $title;
     private $description;
-    private $positionOrder;
     private $components;
-    
+
     public function validate() {
-        
+        if (empty($this->phaseNumber)) {
+            array_push($this->validationMessages, "- {$this->getAttributeNames()['phaseNumber']} should be defined");
+        }
+
+        if (empty($this->title)) {
+            array_push($this->validationMessages, "- {$this->getAttributeNames()['title']} should be defined");
+        }
+
+        if (empty($this->description)) {
+            array_push($this->validationMessages, "- {$this->getAttributeNames()['description']} should be defined");
+        }
+
+        if ($this->validationMode == self::VALIDATION_MODE_INITIAL && count($this->components) == 0) {
+            array_push($this->validationMessages, "- {$this->getAttributeNames()['components']} should be defined");
+        }
+        return count($this->validationMessages) == 0;
+    }
+    
+    public function bindValuesUsingArray(array $valueArray) {
+        if(array_key_exists('components', $valueArray)){
+            $components = explode("+", $this->components);
+            foreach($components as $component){
+                $data = new Component();
+                $data->description = $component;
+                array_push($this->components, $data);
+            }
+        }
+        parent::bindValuesUsingArray($valueArray, $this);
+    }
+
+    public function getAttributeNames() {
+        return array(
+            'phaseNumber' => 'Phase Number',
+            'title' => 'Phase',
+            'description' => 'Description',
+            'components' => 'Components'
+        );
+    }
+
+    public function isNew() {
+        return empty($this->id);
     }
 
     public function __set($name, $value) {
         $this->$name = $value;
     }
-    
+
     public function __get($name) {
         return $this->$name;
     }
+
 }
