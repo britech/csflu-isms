4ffaa0742323a797dc3489299d263cb863c53c81
britech <brian.angyang@gmail.com>
2015-02-19 19:47:28 +0800

    Design Add-on
    added environment status to facilitate the different conditions that may
    be used on the course of development

--- protected/models/ubt/LeadMeasure.php
+++ protected/models/ubt/LeadMeasure.php
@@ -11,14 +11,21 @@ use org\csflu\isms\core\Model;
  * @property String $description
  * @property String $baselineFigure
  * @property String $targetFigure
+ * @property String $leadMeasureEnvironmentStatus
  * @author britech
  */
 class LeadMeasure extends Model {
 
+    const STATUS_ACTIVE = "A";
+    const STATUS_INACTIVE = "I";
+    const STATUS_STOPPED = "S";
+    const STATUS_COMPLETE = "C";
+    
     private $id;
     private $description;
     private $baselineFigure;
     private $targetFigure;
+    private $leadMeasureEnvironmentStatus = self::STATUS_ACTIVE;
 
     public function validate() {
         
