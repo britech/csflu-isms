822503759b7d39207d94a25aed13b18f97a835ca
britech <brian.angyang@gmail.com>
2015-02-21 13:40:24 +0800

    New Fixture
    - added method to list the different status types of the Lead Measure
    - added method to translate the status type code of Lead Measure

--- protected/models/ubt/LeadMeasure.php
+++ protected/models/ubt/LeadMeasure.php
@@ -20,12 +20,28 @@ class LeadMeasure extends Model {
     const STATUS_INACTIVE = "I";
     const STATUS_STOPPED = "S";
     const STATUS_COMPLETE = "C";
-    
+
     private $id;
     private $description;
     private $baselineFigure;
     private $targetFigure;
     private $leadMeasureEnvironmentStatus = self::STATUS_ACTIVE;
+
+    public static function listEnvironmentStatus() {
+        return array(
+            self::STATUS_ACTIVE => 'Active',
+            self::STATUS_INACTIVE => 'Inactive',
+            self::STATUS_STOPPED => 'Stopped/Dropped',
+            self::STATUS_COMPLETE => 'Completed'
+        );
+    }
+
+    public static function translateEnvironmentStatus($environmentStatusCode) {
+        if (array_key_exists($environmentStatusCode, self::listEnvironmentStatus())) {
+            return self::listEnvironmentStatus()[$environmentStatusCode];
+        }
+        return null;
+    }
 
     public function validate() {
         
@@ -39,7 +55,7 @@ class LeadMeasure extends Model {
     public function isNew() {
         return empty($this->id);
     }
-    
+
     public function __set($name, $value) {
         $this->$name = $value;
     }
