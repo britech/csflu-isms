0fbccdc1419c2fb8d54689cb9c406bd44d09b77e
bbangyang <brian.angyang@orangeandbronze.com>
2015-03-29 17:57:22 +0800

    updated link definitions

--- protected/views/wig/_view-navi.php
+++ protected/views/wig/_view-navi.php
@@ -12,7 +12,7 @@ use org\csflu\isms\models\ubt\WigSession;
                 
             <?php if ($data->wigMeetingEnvironmentStatus == WigSession::STATUS_OPEN): ?>
                 <li><?php echo ApplicationUtils::generateLink('#', 'Update Timeline', array('id'=>"update-{$data->id}")); ?></li>
-                <li><?php echo ApplicationUtils::generateLink(array('commitment/create', 'wig' => $data->id), 'Declare Commitments'); ?></li>
+                
 
                 <?php if (count($data->commitments) == 0 and is_null($data->movementUpdate)): ?>
                     <li><?php echo ApplicationUtils::generateLink('#', 'Delete WIG Session', array('id' => "remove-{$data->id}")) ?></li>
@@ -21,7 +21,6 @@ use org\csflu\isms\models\ubt\WigSession;
                 <?php endif; ?>
 
             <?php else: ?>
-                <li><?php echo ApplicationUtils::generateLink(array('wig/commitments', 'wig' => $data->id), 'View Commitments'); ?></li>
                 <li><?php echo ApplicationUtils::generateLink(array('ubt/scoreboard', 'wig' => $data->id), 'View Scoreboard Update'); ?></li>
             <?php endif; ?>
                 
