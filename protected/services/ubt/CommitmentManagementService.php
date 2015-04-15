230890b7f958fd21d98c0c804e28280f99a0f003
britech <brian.angyang@gmail.com>
2015-04-15 15:59:15 +0800

    updated documentation

--- protected/services/ubt/CommitmentManagementService.php
+++ protected/services/ubt/CommitmentManagementService.php
@@ -3,6 +3,7 @@
 namespace org\csflu\isms\service\ubt;
 
 use org\csflu\isms\models\ubt\WigSession;
+use org\csflu\isms\models\ubt\Commitment;
 use org\csflu\isms\exceptions\ServiceException;
 
 /**
@@ -14,6 +15,7 @@ interface CommitmentManagementService {
     /**
      * Inserts the commitments through a WigSession entity
      * @param WigSession $wigSession
+     * @return Commitment[]
      * @throws ServiceException
      */
     public function insertCommitments(WigSession $wigSession);
