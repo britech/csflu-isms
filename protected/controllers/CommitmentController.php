fb0d22c6cee31fd35228aa056c65532d4f0c74fe
britech <brian.angyang@gmail.com>
2015-04-15 19:07:54 +0800

    New Feature
    implemented enlistment facility of the Commitment entity

--- protected/controllers/CommitmentController.php
+++ protected/controllers/CommitmentController.php
@@ -5,11 +5,13 @@ namespace org\csflu\isms\controllers;
 use org\csflu\isms\core\Controller;
 use org\csflu\isms\core\ApplicationConstants;
 use org\csflu\isms\util\ApplicationUtils;
+use org\csflu\isms\exceptions\ServiceException;
 use org\csflu\isms\models\ubt\Commitment;
 use org\csflu\isms\models\commons\Department;
 use org\csflu\isms\models\ubt\WigSession;
 use org\csflu\isms\service\ubt\UnitBreakthroughManagementServiceSimpleImpl;
 use org\csflu\isms\service\uam\SimpleUserManagementServiceImpl;
+use org\csflu\isms\service\ubt\CommitmentManagementServiceSimpleImpl;
 
 /**
  * Description of CommitmentController
@@ -20,6 +22,7 @@ class CommitmentController extends Controller {
 
     private $ubtService;
     private $userService;
+    private $commitmentService;
     private $logger;
 
     public function __construct() {
@@ -27,6 +30,7 @@ class CommitmentController extends Controller {
         $this->logger = \Logger::getLogger(__CLASS__);
         $this->ubtService = new UnitBreakthroughManagementServiceSimpleImpl();
         $this->userService = new SimpleUserManagementServiceImpl();
+        $this->commitmentService = new CommitmentManagementServiceSimpleImpl();
     }
 
     public function enlist() {
@@ -46,6 +50,46 @@ class CommitmentController extends Controller {
         $this->unsetSessionData('validation');
     }
 
+    public function insert() {
+        $this->validatePostData(array('Commitment', 'WigSession', 'UserAccount'));
+
+        $wigSessionData = $this->getFormData('WigSession');
+        $userAccountData = $this->getFormData('UserAccount');
+        $commitmentData = $this->getFormData('Commitment');
+
+        $wigSession = new WigSession();
+        $wigSession->bindValuesUsingArray(array('wigsession' => $wigSessionData), $wigSession);
+
+        $commitmentList = explode('+', $commitmentData['commitment']);
+        $commitments = array();
+        foreach ($commitmentList as $commitmentEntry) {
+            $commitment = new Commitment();
+            $commitment->bindValuesUsingArray(array('user' => $userAccountData, 
+                'commitment' => array('commitment' => $commitmentEntry)));
+            $commitments = array_merge($commitments, array($commitment));
+        }
+        $wigSession->commitments = $commitments;
+        
+        if(count($wigSession->commitments) == 0){
+            $this->setSessionData('validation', array('Commitments should be defined'));
+            $this->redirect(array('commitment/enlist'));
+        }
+        
+        try {
+            $commitmentsEnlisted = $this->commitmentService->insertCommitments($wigSession);
+            $commitmentsToDisplay = array();
+            foreach($commitmentsEnlisted as $enlistedCommitment){
+                $commitmentsToDisplay = array_merge($commitmentsToDisplay, array($enlistedCommitment->commitment));
+            }
+            $this->setSessionData('notif', array('class'=>'success', 'message'=>'Commitment/s enlisted<br/>' . implode('<br/>', $commitmentsToDisplay)));
+            $this->redirect(array('ip/index'));
+        } catch (ServiceException $ex) {
+            $this->logger->error($ex->getMessage(), e);
+            $this->setSessionData('validation', array($ex->getMessage()));
+            $this->redirect(array('commitment/enlist'));
+        }
+    }
+
     private function loadAccountModel($remote = false) {
         $account = $this->userService->getAccountById($this->getSessionData('user'));
         if (is_null($account->id)) {
@@ -61,7 +105,6 @@ class CommitmentController extends Controller {
     }
 
     private function loadWigSession(Department $department, $remote = false) {
-        $this->logger->debug($department);
         $unitBreakthroughs = $this->ubtService->listUnitBreakthrough(null, $department, true);
         if (count($unitBreakthroughs) == 0) {
             $url = array('ubt/manage');
