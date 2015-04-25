7035c072a6bde9be9be0d2f3f68eba6e131d094e
britech <brian.angyang@gmail.com>
2015-04-25 18:19:04 +0800

    added entity validation

--- protected/models/ubt/CommitmentMovement.php
+++ protected/models/ubt/CommitmentMovement.php
@@ -7,19 +7,25 @@ use org\csflu\isms\core\Model;
 /**
  * Description of CommitmentMovement
  *
- * @property String $progressFigure
+ * @property String $movementFigure
  * @property String $notes
  * @property \DateTime $dateCaptured
  * @author britech
  */
 class CommitmentMovement extends Model {
 
-    private $progressFigure;
+    private $movementFigure;
     private $notes;
     private $dateCaptured;
 
     public function validate() {
+        if(strlen($this->progressFigure) == 0){
+            array_push($this->validationMessages, '- Movement Figure should be defined');
+        }
         
+        if(strlen($this->notes) == 0){
+            array_push($this->validationMessages, '- Notes should be defined');
+        }
     }
 
     public function __set($name, $value) {
