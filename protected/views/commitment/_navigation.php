9558df2ff014d9bac19610a46ef3d985b43683de
britech <brian.angyang@gmail.com>
2015-04-25 13:26:26 +0800

    updated link definitions if the commitment is set to pending

--- protected/views/commitment/_navigation.php
+++ protected/views/commitment/_navigation.php
@@ -10,6 +10,7 @@ use org\csflu\isms\models\ubt\Commitment;
         <li class="heading"><?php echo ApplicationUtils::generateLink('#', 'Actions', array('style' => 'padding-left: 0px;')); ?></li>
             <?php if ($data->commitmentEnvironmentStatus == Commitment::STATUS_PENDING): ?>
             <li><?php echo ApplicationUtils::generateLink('#', 'Set as Ongoing', array('id' => "pending-{$data->id}")); ?></li>
+            <li><?php echo ApplicationUtils::generateLink(array('commitment/finish', 'id' => $data->id), 'Set To Finished'); ?></li>
             <?php if (!isset($data->commitmentMovements) || count($data->commitmentMovements) == 0): ?>
                 <li><?php echo ApplicationUtils::generateLink('#', 'Delete Commitment', array('id' => "remove-{$data->id}")); ?></li>
             <?php endif; ?>
