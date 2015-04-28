d12789d954c1a938fb01e0a4fb956a42fd2b84ff
britech <brian.angyang@gmail.com>
2015-04-28 17:40:15 +0800

    updated form layout

--- protected/views/ubt/form.php
+++ protected/views/ubt/form.php
@@ -10,26 +10,20 @@ $form = new Form(array(
     'hasFieldset' => true
         ));
 ?>
-<link href="assets/flick/jquery-ui-1.10.4.custom.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/tag-editor/jquery.tag-editor.css" rel="stylesheet" type="text/css"/>
-<script type="text/javascript" src="assets/jquery/jquery-ui-1.10.4.custom.js"></script>
-<script type="text/javascript" src="assets/tag-editor/jquery.tag-editor.js"></script>
 <script type="text/javascript" src="protected/js/ubt/form.js"></script>
-
-<?php echo $form->startComponent(); ?>
-<?php echo $form->constructHeader($model->isNew() ? 'Create Unit Breakthrough' : 'Update Unit Breakthrough'); ?>
-<div class="ink-alert basic info">
-    <strong>Important Note:</strong>&nbsp;Fields with * are required.
-</div>
-<div id="validation-container"></div>
-<?php
-if (isset($params['validation']) && !empty($params['validation'])) {
-    $this->viewWarningPage('Validation error/s. Please check your entries', implode('<br/>', $params['validation']));
-}
-?>
 <div class="column-group quarter-gutters">
-    <!-- start partition 1-->
-    <div class="all-50">
+    <div class="all-60 push-center">
+        <?php echo $form->startComponent(); ?>
+        <?php echo $form->constructHeader($model->isNew() ? 'Create Unit Breakthrough' : 'Update Unit Breakthrough'); ?>
+        <div class="ink-alert basic info">
+            <strong>Important Note:</strong>&nbsp;Fields with * are required.
+        </div>
+        <div id="validation-container"></div>
+        <?php
+        if (isset($params['validation']) && !empty($params['validation'])) {
+            $this->viewWarningPage('Validation error/s. Please check your entries', implode('<br/>', $params['validation']));
+        }
+        ?>
         <div class="control-group">
             <?php echo $form->renderLabel($model, 'unit', array('required' => true)); ?>
             <div class="control">
@@ -46,20 +40,15 @@ if (isset($params['validation']) && !empty($params['validation'])) {
             <label>Timeline&nbsp;*</label>
             <div class="control">
                 <div id="timeline-input"></div>
-                <?php echo $form->renderSubmitButton('Create', array('class' => 'ink-button green flat', 'style' => 'margin-top: 1em; margin-left: 0px;')); ?>
+
             </div>
         </div>
-    </div>
-    <!-- end partition 1-->
-
-    <!-- start partition 2-->
-    <div class="all-50">
         <div class="column-group quarter-gutters">
             <div class="all-50">
                 <div class="control-group">
                     <label><?php echo $form->renderLabel($model, 'baselineFigure', array('required' => true)); ?></label>
                     <div class="control">
-                       <div id="baseline-input"></div>
+                        <div id="baseline-input"></div>
                     </div>
                 </div>
             </div>
@@ -90,22 +79,21 @@ if (isset($params['validation']) && !empty($params['validation'])) {
             <?php echo $form->renderLabel($model, 'measures', array('required' => true)); ?>
             <div class="control">
                 <div id="measures-input"></div>
+                <?php echo $form->renderSubmitButton('Create', array('class' => 'ink-button green flat', 'style' => 'margin-top: 1em; margin-left: 0px;')); ?>
             </div>
         </div>
     </div>
-    <!-- end partition 2-->
+    <?php echo $form->renderHiddenField($model, 'validationMode'); ?>
+    <?php echo $form->renderHiddenField($model, 'startingPeriod', array('id' => 'ubt-start')); ?>
+    <?php echo $form->renderHiddenField($model, 'endingPeriod', array('id' => 'ubt-end')); ?>
+    <?php echo $form->renderHiddenField($model, 'baselineFigure', array('id' => 'baseline')); ?>
+    <?php echo $form->renderHiddenField($model, 'targetFigure', array('id' => 'target')); ?>
+    <?php echo $form->renderHiddenField($uomModel, 'id', array('id' => 'uom')); ?>
+    <?php echo $form->renderHiddenField($departmentModel, 'id', array('id' => 'department')); ?>
+    <?php echo $form->renderHiddenField($objectiveModel, 'id', array('id' => 'objectives')); ?>
+    <?php echo $form->renderHiddenField($measureProfileModel, 'id', array('id' => 'measures')); ?>
+    <?php echo $form->renderHiddenField($mapModel, 'id', array('id' => 'map')); ?>
+    <?php echo $form->renderHiddenField($mapModel, 'startingPeriodDate', array('id' => 'map-start')); ?>
+    <?php echo $form->renderHiddenField($mapModel, 'endingPeriodDate', array('id' => 'map-end')); ?>
+    <?php echo $form->endComponent(); ?>
 </div>
-<?php echo $form->renderHiddenField($model, 'validationMode'); ?>
-<?php echo $form->renderHiddenField($model, 'startingPeriod', array('id' => 'ubt-start')); ?>
-<?php echo $form->renderHiddenField($model, 'endingPeriod', array('id' => 'ubt-end')); ?>
-<?php echo $form->renderHiddenField($model, 'baselineFigure', array('id' => 'baseline')); ?>
-<?php echo $form->renderHiddenField($model, 'targetFigure', array('id' => 'target')); ?>
-<?php echo $form->renderHiddenField($uomModel, 'id', array('id' => 'uom')); ?>
-<?php echo $form->renderHiddenField($departmentModel, 'id', array('id' => 'department')); ?>
-<?php echo $form->renderHiddenField($objectiveModel, 'id', array('id' => 'objectives')); ?>
-<?php echo $form->renderHiddenField($measureProfileModel, 'id', array('id' => 'measures')); ?>
-<?php echo $form->renderHiddenField($mapModel, 'id', array('id' => 'map')); ?>
-<?php echo $form->renderHiddenField($mapModel, 'startingPeriodDate', array('id' => 'map-start')); ?>
-<?php echo $form->renderHiddenField($mapModel, 'endingPeriodDate', array('id' => 'map-end')); ?>
-<?php
-echo $form->endComponent();
