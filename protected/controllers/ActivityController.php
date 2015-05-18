46669e37f4e37440d5b3196257d0635f58b57f35
britech <brian.angyang@gmail.com>
2015-05-18 11:55:19 +0800

    New Feature
    added handling for an activity entity to be finished

--- protected/controllers/ActivityController.php
+++ protected/controllers/ActivityController.php
@@ -102,6 +102,7 @@ class ActivityController extends Controller {
             $this->setSessionData('notif', array('class' => 'info', 'message' => "{$activity->title} set to {$activity->translateStatusCode($status)}"));
         } catch (ServiceException $ex) {
             $this->logger->error($ex->getMessage(), $ex);
+            $this->setSessionData('notif', array('class' => 'error', 'message' => 'An error occured during Activity status update'));
         }
         $this->renderAjaxJsonResponse(array('url' => ApplicationUtils::resolveUrl(array('activity/index', 'initiative' => $initiative->id, 'period' => $period))));
     }
@@ -157,6 +158,80 @@ class ActivityController extends Controller {
         $this->logRevision(RevisionHistory::TYPE_INSERT, ModuleAction::MODULE_INITIATIVE, $initiative->id, $activityMovement);
         $this->setSessionData('notif', array('class' => 'success', 'message' => "Activity Movement succesfully logged"));
         $this->redirect(array('activity/manage', 'id' => $activity->id));
+    }
+
+    public function finish($id = null) {
+        if (is_null($id)) {
+            $this->processFormInput();
+        }
+
+        $activity = $this->loadModel($id);
+        $activity->activityEnvironmentStatus = Activity::STATUS_FINISHED;
+        $initiative = $this->loadInitiativeModel(null, $activity);
+        $this->title = ApplicationConstants::APP_NAME . ' - Finish Activity';
+        $this->render('activity/enlist', array(
+            'breadcrumb' => array(
+                'Home' => array('site/index'),
+                'Manage Initiatives' => array('initiative/manage'),
+                'Activity Dashboard' => array('activity/index', 'initiative' => $initiative->id, 'period' => $activity->startingPeriod->format('Y-m')),
+                'Manage Activity' => array('activity/manage', 'id' => $activity->id),
+                'Set Activity to Finished' => 'active'
+            ),
+            'model' => new ActivityMovement(),
+            'activity' => $activity,
+            'action' => array('activity/finish'),
+            'header' => 'Set Activity to Finished',
+            'validation' => $this->getSessionData('validation')
+        ));
+        $this->unsetSessionData('validation');
+    }
+
+    private function processFormInput() {
+        $this->validatePostData(array('ActivityMovement', 'Activity'));
+
+        $activityMovementEntry = $this->controllerSupport->constructActivityMovementEntity();
+        if (!$activityMovementEntry->validate()) {
+            $this->setSessionData('validation', $activityMovementEntry->validationMessages);
+            $this->resolveFormRedirect();
+        }
+
+        $activityData = $this->getFormData('Activity');
+        $activity = new Activity();
+        $activity->bindValuesUsingArray(array(
+            'activity' => $activityData
+        ));
+        $activityMovementStatus = $this->controllerSupport->constructActivityMovementEntity($activity->activityEnvironmentStatus);
+        $activity->movements = array($activityMovementStatus, $activityMovementEntry);
+
+        $model = $this->loadModel($activity->id);
+        $model->activityEnvironmentStatus = $activity->activityEnvironmentStatus;
+        $initiative = $this->loadInitiativeModel(null, $model);
+
+        try {
+            $this->initiativeService->updateActivity($model, $this->modelLoaderUtil->loadComponentModel(null, $model));
+            $this->logCustomRevision(RevisionHistory::TYPE_UPDATE, ModuleAction::MODULE_INITIATIVE, $initiative->id, "[Activity Status Update]\n\nActivity:\t{$model->title}\nStatus:\t{$model->translateStatusCode()}");
+            $this->initiativeService->insertActivityMovement($activity);
+            $this->logEnlistedMovements($initiative, $activity->movements);
+            $this->setSessionData('notif', array('class' => 'info', 'message' => "{$model->title} set to {$model->translateStatusCode()}"));
+        } catch (ServiceException $ex) {
+            $this->logger->error($ex->getMessage(), $ex);
+            $this->setSessionData('notif', array('class' => 'error', 'message' => 'An error occured while Activity Movement is being logged'));
+        }
+        $this->redirect(array('activity/index', 'initiative' => $initiative->id, 'period' => $model->startingPeriod->format('Y-m')));
+    }
+
+    private function resolveFormRedirect() {
+        $activityData = $this->getFormData('Activity');
+        $id = $activityData['id'];
+        $status = $activityData['activityEnvironmentStatus'];
+        if ($status == Activity::STATUS_FINISHED) {
+            $this->redirect(array('activity/finish', 'id' => $id));
+        } elseif ($status == Activity::STATUS_UNFINISHED) {
+            /**
+             * @todo handle unfinish redirect
+             */
+        }
+        return;
     }
 
     private function logEnlistedMovements(Initiative $initiative, array $movements) {
