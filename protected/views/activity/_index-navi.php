791fe57df9624b1dd7986709ce0670247387ca73
britech <brian.angyang@gmail.com>
2015-05-23 14:38:04 +0800

    updated link description

--- protected/views/activity/_index-navi.php
+++ protected/views/activity/_index-navi.php
@@ -7,6 +7,6 @@ use org\csflu\isms\util\ApplicationUtils;
 <div class="ink-navigation">
     <ul class="menu vertical">
         <li class="heading"><?php echo ApplicationUtils::generateLink('#', 'Action', array('style' => 'padding-left:0px;')) ?></li>
-        <li><?php echo ApplicationUtils::generateLink(array('initiative/generateInitiativeUpdate', 'id' => $data->id, 'period' => $date->format('Y-m')), 'Initiative Update Report'); ?></li>
+        <li><?php echo ApplicationUtils::generateLink(array('initiative/generateUpdateReport', 'id' => $data->id, 'period' => $date->format('Y-m')), 'Initiative Update Report'); ?></li>
     </ul>
 </div>
