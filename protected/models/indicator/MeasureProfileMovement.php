4a49bccb6ecb68d137347644a31a9a927a1f0607
britech <brian.angyang@gmail.com>
2015-05-26 13:46:01 +0800

    New Fixture
    - updated entity validation
    - overriden the value-binding and attribute naming methods

--- protected/models/indicator/MeasureProfileMovement.php
+++ protected/models/indicator/MeasureProfileMovement.php
@@ -19,7 +19,27 @@ class MeasureProfileMovement extends Model {
     private $movementLogs;
 
     public function validate() {
-        
+        if (!$this->periodDate instanceof \DateTime) {
+            array_push($this->validationMessages, '- Time Period should be defined');
+        }
+
+        if (!strlen($this->movementValue) > 0) {
+            array_push($this->validationMessages, '- Movement value should be defined');
+        }
+
+        return count($this->validationMessages) == 0;
+    }
+
+    public function bindValuesUsingArray(array $valueArray) {
+        parent::bindValuesUsingArray($valueArray, $this);
+        $this->periodDate = \DateTime::createFromFormat('Y-m-d', $this->periodDate);
+    }
+
+    public function getAttributeNames() {
+        return array(
+            'periodDate' => 'Time Period',
+            'movementValue' => 'Movement Value'
+        );
     }
 
     public function __set($name, $value) {
--- protected/models/indicator/MeasureProfileMovementLog.php
+++ protected/models/indicator/MeasureProfileMovementLog.php
@@ -20,7 +20,30 @@ class MeasureProfileMovementLog extends Model {
     private $dateEntered;
 
     public function validate() {
-        
+        if (!$this->user instanceof UserAccount) {
+            array_push($this->validationMessages, '- User should be defined');
+        }
+
+        if (!strlen($this->notes) > 0) {
+            array_push($this->validationMessages, '- Notes should be defined');
+        }
+
+        return count($this->validationMessages) == 0;
+    }
+
+    public function bindValuesUsingArray(array $valueArray) {
+        if (array_key_exists('user', $valueArray)) {
+            $this->user = new UserAccount();
+            $this->user->id = $valueArray['user']['id'];
+        }
+        parent::bindValuesUsingArray($valueArray, $this);
+    }
+
+    public function getAttributeNames() {
+        return array(
+            'user' => 'User',
+            'notes' => 'Remarks'
+        );
     }
 
     public function __set($name, $value) {
