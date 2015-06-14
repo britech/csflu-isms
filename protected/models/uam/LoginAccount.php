2346caaaf93916e600193d757fd192afb41676be
britech <brian.angyang@gmail.com>
2015-06-14 12:15:26 +0800

    Enhancement
    added utility method which lists and translate the status code of the
    LoginAccount component

--- protected/models/uam/LoginAccount.php
+++ protected/models/uam/LoginAccount.php
@@ -3,6 +3,7 @@
 namespace org\csflu\isms\models\uam;
 
 use org\csflu\isms\core\Model;
+
 /**
  * Description of LoginAccount
  *
@@ -11,24 +12,40 @@ use org\csflu\isms\core\Model;
  * @property Integer $status
  * @author britech
  */
-class LoginAccount extends Model{
-    
+class LoginAccount extends Model {
+
     const STATUS_ACTIVE = 1;
     const STATUS_DISABLED = 0;
-    
+
     private $username;
     private $password;
     private $status;
-    
+
     public function validate() {
         
     }
+
+    public static function listStatusTypes() {
+        return array(
+            self::STATUS_ACTIVE => 'Active',
+            self::STATUS_DISABLED => 'Inactive'
+        );
+    }
     
+    public static function translateStatusType($statusType = null){
+        $type = is_null($statusType) ? $this->status : $statusType;
+        if(array_key_exists($type, self::listStatusTypes())){
+            return self::listStatusTypes()[$type];
+        }
+        return "Undefined";
+    }
+
     public function __set($name, $value) {
         $this->$name = $value;
     }
-    
+
     public function __get($name) {
         return $this->$name;
     }
+
 }
