704cb4702023d734fa7880637b1ca40c39a10ddb
britech <brian.angyang@gmail.com>
2015-06-14 11:09:16 +0800

    New Feature
    added method argument which excludes the enlisted departments based on
    the list of accounts

--- protected/services/commons/DepartmentService.php
+++ protected/services/commons/DepartmentService.php
@@ -4,12 +4,14 @@ namespace org\csflu\isms\service\commons;
 
 use org\csflu\isms\exceptions\ServiceException;
 use org\csflu\isms\models\commons\Department;
+use org\csflu\isms\models\uam\UserAccount;
+
 /**
  *
  * @author britech
  */
 interface DepartmentService {
-    
+
     /**
      * Retrieves the department detail via array.
      * Allowed parameter keys are 'id' or 'code'
@@ -18,22 +20,23 @@ interface DepartmentService {
      * @throws ServiceException
      */
     public function getDepartmentDetail(array $params);
-    
+
     /**
      * Retrieves the defined departments
-     * @param Integer $excludeDepartmentId Optional, the department not to be included on the list
+     * @param int $excludeDepartmentId Optional, the department not to be included on the list
+     * @param UserAccount[] $accounts Optional, the departments not to be included on the list
      * @return Department
      * @throws ServiceException
      */
-    public function listDepartments($excludeDepartmentId = null);
-    
+    public function listDepartments($excludeDepartmentId = null, array $accounts = null);
+
     /**
      * Enlists a department
      * @param Department $department
      * @throws ServiceException
      */
     public function enlistDepartment($department);
-    
+
     /**
      * Updates a department
      * @param Department $department
--- protected/services/commons/DepartmentServiceSimpleImpl.php
+++ protected/services/commons/DepartmentServiceSimpleImpl.php
@@ -5,38 +5,61 @@ namespace org\csflu\isms\service\commons;
 use org\csflu\isms\service\commons\DepartmentService;
 use org\csflu\isms\dao\commons\DepartmentDaoSqlImpl as DepartmentDao;
 use org\csflu\isms\exceptions\ServiceException;
+
 /**
  * Description of DepartmentServiceSimpleImpl
  *
  * @author britech
  */
-class DepartmentServiceSimpleImpl implements DepartmentService{
-    
+class DepartmentServiceSimpleImpl implements DepartmentService {
+
     private $daoSource = null;
+
     public function __construct() {
         $this->daoSource = new DepartmentDao();
     }
 
     public function getDepartmentDetail(array $params) {
-        if(array_key_exists('id', $params)){
+        if (array_key_exists('id', $params)) {
             return $this->daoSource->getDepartmentById($params['id']);
-        } else if(array_key_exists('code', $params)){
+        } else if (array_key_exists('code', $params)) {
             return $this->daoSource->getDepartmentByCode($params['code']);
-        } else{
+        } else {
             throw new ServiceException('Identifier expected');
         }
     }
 
-    public function listDepartments($excludeDepartmentId = null) {
-        if(!is_null($excludeDepartmentId)){
+    public function listDepartments($excludeDepartmentId = null, array $accounts = null) {
+        if (!is_null($excludeDepartmentId)) {
             $departments = $this->daoSource->listDepartments($excludeDepartmentId);
+        } elseif (!is_null($accounts)) {
+            $departments = $this->resolveDepartmentListingByAccounts($accounts);
         } else {
             $departments = $this->daoSource->listDepartments();
         }
-        if(count($departments) == 0){
+        if (count($departments) == 0) {
             throw new ServiceException('No departments defined');
         }
         return $departments;
+    }
+
+    private function resolveDepartmentListingByAccounts(array $accounts) {
+        $filteredDepartments = array();
+        $departments = $this->daoSource->listDepartments();
+        foreach ($departments as $department) {
+            $found = false;
+            foreach ($accounts as $account) {
+                if ($department == $account->employee->department) {
+                    $found = true;
+                    break;
+                }
+            }
+
+            if (!$found) {
+                $filteredDepartments = array_merge($filteredDepartments, array($department));
+            }
+        }
+        return $filteredDepartments;
     }
 
     public function enlistDepartment($department) {
