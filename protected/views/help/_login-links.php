da10cfdb0287b89205936405543157564bff32c2
britech <brian.angyang@gmail.com>
2015-06-20 16:11:55 +0800

    Updated Fixture
    updated the width of the navigation links

--- protected/views/help/_login-links.php
+++ protected/views/help/_login-links.php
@@ -4,7 +4,7 @@ namespace org\csflu\isms\views;
 
 use org\csflu\isms\util\ApplicationUtils;
 ?>
-<div style="position: fixed; width: 200px;">
+<div style="position: fixed; width: 230px;">
     <div class="ink-navigation">
         <ul class="menu vertical" style="margin-top: 0px;">
             <li class="heading"><?php echo ApplicationUtils::generateLink('#', 'Contents', array('style' => 'padding-left:0px;')) ?></li>
