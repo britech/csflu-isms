dce27878664a3bebf1ee3fc9ce4ad6aec20d2d8a
britech <brian.angyang@gmail.com>
2015-06-23 17:18:57 +0800

    Updated Fixture
    updated the font size of the help contents in the initiative module

--- protected/views/help/_initiative-links.php
+++ protected/views/help/_initiative-links.php
@@ -8,15 +8,16 @@ use org\csflu\isms\util\ApplicationUtils;
     <div class="ink-navigation">
         <ul class="menu vertical" style="margin-top: 0px;">
             <li class="heading"><?php echo ApplicationUtils::generateLink('#', 'Contents', array('style' => 'padding-left:0px;')) ?></li>
-            <li><?php echo ApplicationUtils::generateLink(array('site/index'), 'Back to Application'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#create', 'Initiative Enlistment'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#entryUpdate', 'Update Initiative Entry Data'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#manageOffice', 'Manage Implementing Offices'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#manageAlignment', 'Manage Strategy Alignments'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#managePhase', 'Manage Phases'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#manageComponent', 'Manage Components'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#manageActivity', 'Manage Activity'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#movementUpdate', 'Initiative Update'); ?></li>
+            <li style="font-size: 15px;"><?php echo ApplicationUtils::generateLink(array('site/index'), 'Back to Application'); ?></li>
+            <li style="font-size: 15px;"><?php echo ApplicationUtils::generateLink('#create', 'Initiative Enlistment'); ?></li>
+            <li style="font-size: 15px;"><?php echo ApplicationUtils::generateLink('#entryUpdate', 'Update Initiative Entry Data'); ?></li>
+            <li style="font-size: 15px;"><?php echo ApplicationUtils::generateLink('#manageOffice', 'Manage Implementing Offices'); ?></li>
+            <li style="font-size: 15px;"><?php echo ApplicationUtils::generateLink('#manageAlignment', 'Manage Strategy Alignments'); ?></li>
+            <li style="font-size: 15px;"><?php echo ApplicationUtils::generateLink('#managePhase', 'Manage Phases'); ?></li>
+            <li style="font-size: 15px;"><?php echo ApplicationUtils::generateLink('#manageComponent', 'Manage Components'); ?></li>
+            <li style="font-size: 15px;"><?php echo ApplicationUtils::generateLink('#manageActivity', 'Manage Activity'); ?></li>
+            <li style="font-size: 15px;"><?php echo ApplicationUtils::generateLink('#activityDashboard', 'Managing the Activity Dashboard'); ?></li>
+            <li style="font-size: 15px;"><?php echo ApplicationUtils::generateLink('#movementUpdate', 'Initiative Update'); ?></li>
         </ul>
     </div>
 </div>
