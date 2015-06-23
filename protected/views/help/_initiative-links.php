9e3c3cbc7a4d7092762e97e00f3ac49ad9fe8086
britech <brian.angyang@gmail.com>
2015-06-23 10:50:57 +0800

    updated link description

--- protected/views/help/_initiative-links.php
+++ protected/views/help/_initiative-links.php
@@ -10,12 +10,12 @@ use org\csflu\isms\util\ApplicationUtils;
             <li class="heading"><?php echo ApplicationUtils::generateLink('#', 'Contents', array('style' => 'padding-left:0px;')) ?></li>
             <li><?php echo ApplicationUtils::generateLink(array('site/index'), 'Back to Application'); ?></li>
             <li><?php echo ApplicationUtils::generateLink('#create', 'Initiative Enlistment'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#entryUpdate', 'Updating Initiative Entry Data'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#manageOffice', 'Managing Implementing Offices'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#manageAlignment', 'Managing Strategy Alignments'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#managePhase', 'Managing Phases'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#manageComponent', 'Managing Components'); ?></li>
-            <li><?php echo ApplicationUtils::generateLink('#manageActivity', 'Managing Activity'); ?></li>
+            <li><?php echo ApplicationUtils::generateLink('#entryUpdate', 'Update Initiative Entry Data'); ?></li>
+            <li><?php echo ApplicationUtils::generateLink('#manageOffice', 'Manage Implementing Offices'); ?></li>
+            <li><?php echo ApplicationUtils::generateLink('#manageAlignment', 'Manage Strategy Alignments'); ?></li>
+            <li><?php echo ApplicationUtils::generateLink('#managePhase', 'Manage Phases'); ?></li>
+            <li><?php echo ApplicationUtils::generateLink('#manageComponent', 'Manage Components'); ?></li>
+            <li><?php echo ApplicationUtils::generateLink('#manageActivity', 'Manage Activity'); ?></li>
             <li><?php echo ApplicationUtils::generateLink('#movementUpdate', 'Initiative Update'); ?></li>
         </ul>
     </div>
