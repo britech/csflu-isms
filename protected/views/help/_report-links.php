731a311f1b14851a0859e3fb3cf02715241548a3
britech <brian.angyang@gmail.com>
2015-06-24 16:13:23 +0800

    New Feature
    added help files for reports generation-scorecard update scenario

--- protected/views/help/_report-links.php
+++ protected/views/help/_report-links.php
@@ -10,6 +10,7 @@ use org\csflu\isms\util\ApplicationUtils;
             <li class="heading"><?php echo ApplicationUtils::generateLink('#', 'Contents', array('style' => 'padding-left:0px;')) ?></li>
             <li><?php echo ApplicationUtils::generateLink(array('site/index'), 'Back to Application'); ?></li>
             <li><?php echo ApplicationUtils::generateLink('#measureProfile', 'Measure Profile'); ?></li>
+            <li><?php echo ApplicationUtils::generateLink('#scorecardUpdate', 'Scorecard Update'); ?></li>
             <li><?php echo ApplicationUtils::generateLink('#scorecardTemp', 'Scorecard Template'); ?></li>
             <li><?php echo ApplicationUtils::generateLink('#iniPow', 'Program of Work'); ?></li>
             <li><?php echo ApplicationUtils::generateLink('#iniUpdate', 'Initiative Update Template'); ?></li>
--- protected/views/help/report.php
+++ protected/views/help/report.php
@@ -41,3 +41,44 @@ use org\csflu\isms\util\ApplicationUtils;
     </li>
 </ol>
 <?php echo ApplicationUtils::generateLink('#top', 'Back to Top', array('style' => 'display: block; margin-bottom: 50px;')); ?>
+
+<a name="scorecardUpdate" style="display: block; border-bottom: 1px solid black;">Scorecard Update</a>
+<ol>
+    <li>
+        From your application's Home page, click <strong>Modules&nbsp;&gt;&nbsp;<?php echo ApplicationUtils::generateLink(array('map/index'), 'Strategy Management'); ?></strong>
+        <img src="protected/views/help/images/commons/index.png" alt="step-1"/>
+    </li>
+    <li>
+        Upon clicking the link, you will be redirected to the Strategy Map directory page.
+        <br/>
+        Select the strategy map of the Measure Profile from the directory listing.
+        <img src="protected/views/help/images/map/create-step-1.png" alt="step-2"/>
+    </li>
+    <li>
+        You will be then redirected to the View page of the selected Strategy Map. Click the <strong>Manage Measure Profiles</strong> link.
+        <img src="protected/views/help/images/map/create-step-3.png" alt="step-3"/>
+    </li>
+    <li>
+        Upon clicking the <strong>Manage Measure Profiles</strong> link, you will be redirected to the directory listing of Measure Profiles. 
+        <br/>
+        Click the <strong>Manage Movements</strong> link beside the measure profile that you want to generate the report.
+        <img src="protected/views/help/images/measure-profile/create-step-1.png" alt="step-4"/>
+    </li>
+    <li>
+        Upon clicking the link, the application will prompt for a time period to display the movement of the selected Measure Profile.
+        <img src="protected/views/help/images/measure-profile/mov-step-1.png" alt="step-5"/>
+    </li>
+    <li>
+        You will be then redirected to the movement overview page of the selected Measure Profile under the selected Date Period.
+        <br/>
+        Click the <strong>Generate Scorecard Update</strong> link to generate the report.
+        <img src="protected/views/help/images/measure-profile/mov-step-2.png" alt="step-6"/>
+    </li>
+    <li>
+        Upon clicking the link, the application will process the request and create the report file.
+    </li>
+    <li>
+        DONE.
+    </li>
+</ol>
+<?php echo ApplicationUtils::generateLink('#top', 'Back to Top', array('style' => 'display: block; margin-bottom: 50px;')); ?>
