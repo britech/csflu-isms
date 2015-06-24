1f4a83ed376ddf4ed5808b73633cdab7b1078947
britech <brian.angyang@gmail.com>
2015-06-24 16:34:56 +0800

    New Feature
    added help description for reports generation-scorecard template
    scenario

--- protected/views/help/report.php
+++ protected/views/help/report.php
@@ -82,3 +82,34 @@ use org\csflu\isms\util\ApplicationUtils;
     </li>
 </ol>
 <?php echo ApplicationUtils::generateLink('#top', 'Back to Top', array('style' => 'display: block; margin-bottom: 50px;')); ?>
+
+<a name="scorecardTemp" style="display: block; border-bottom: 1px solid black;">Scorecard Template</a>
+<ol>
+    <li>
+        From your application's Home page, click <strong>Modules&nbsp;&gt;&nbsp;<?php echo ApplicationUtils::generateLink(array('map/index'), 'Strategy Management'); ?></strong>
+        <img src="protected/views/help/images/commons/index.png" alt="step-1"/>
+    </li>
+    <li>
+        Upon clicking the link, you will be redirected to the Strategy Map directory page.
+        <br/>
+        Select the strategy map of the Measure Profile from the directory listing.
+        <img src="protected/views/help/images/map/create-step-1.png" alt="step-2"/>
+    </li>
+    <li>
+        You will be then redirected to the View page of the selected Strategy Map. Click the <strong>Manage Measure Profiles</strong> link.
+        <img src="protected/views/help/images/map/create-step-3.png" alt="step-3"/>
+    </li>
+    <li>
+        Upon clicking the <strong>Manage Measure Profiles</strong> link, you will be redirected to the directory listing of Measure Profiles. 
+        <br/>
+        Click the <strong>Generate Scorecard Template</strong> to generate the report.
+        <img src="protected/views/help/images/measure-profile/create-step-1.png" alt="step-4"/>
+    </li>
+    <li>
+        Upon clicking the link, the application will process the request and create the report file.
+    </li>
+    <li>
+        DONE.
+    </li>
+</ol>
+<?php echo ApplicationUtils::generateLink('#top', 'Back to Top', array('style' => 'display: block; margin-bottom: 50px;')); ?>
