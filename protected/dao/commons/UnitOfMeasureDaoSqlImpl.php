883971ec9ad9cd00d54baae75818b264f80a22a8
britech <brian.angyang@gmail.com>
2015-08-08 20:52:26 +0800

    Refactoring
    - refactored the utilization of database connection object
    - added wrapping of captured exception

--- protected/dao/commons/UnitOfMeasureDaoSqlImpl.php
+++ protected/dao/commons/UnitOfMeasureDaoSqlImpl.php
@@ -5,7 +5,8 @@ namespace org\csflu\isms\dao\commons;
 use org\csflu\isms\dao\commons\UnitOfMeasureDao;
 use org\csflu\isms\exceptions\DataAccessException;
 use org\csflu\isms\models\commons\UnitOfMeasure;
-use org\csflu\isms\core\ConnectionManager;
+use org\csflu\isms\core\DatabaseConnectionManager;
+use org\csflu\isms\util\ApplicationLoggerUtils;
 
 /**
  * Description of UnitOfMeasureDaoSqlImpl
@@ -14,56 +15,71 @@ use org\csflu\isms\core\ConnectionManager;
  */
 class UnitOfMeasureDaoSqlImpl implements UnitOfMeasureDao {
 
+    private $logger;
+    private $db;
+
+    public function __construct() {
+        $this->logger = \Logger::getLogger(__CLASS__);
+        $this->db = DatabaseConnectionManager::getInstance()->getMainDbConnection();
+    }
+
     public function listUnitOfMeasures() {
-        $db = ConnectionManager::getConnectionInstance();
         try {
-            $dbst = $db->prepare('SELECT uom_id, uom_symbol, uom_desc FROM uom ORDER BY uom_desc ASC');
+            $dbst = $this->db->prepare('SELECT uom_id, uom_symbol, uom_desc FROM uom ORDER BY uom_desc ASC');
             $dbst->execute();
+            ApplicationLoggerUtils::logSql($this->logger, $dbst);
 
             $uoms = array();
             while ($data = $dbst->fetch()) {
                 $uom = new UnitOfMeasure();
                 list($uom->id, $uom->symbol, $uom->description) = $data;
-                array_push($uoms, $uom);
+                $uoms = array_merge($uoms, array($uom));
             }
             return $uoms;
         } catch (\PDOException $ex) {
-            throw new DataAccessException($ex->getMessage());
+            throw new DataAccessException($ex->getMessage(), $ex);
         }
     }
 
-    public function enlistUom($uom) {
-        $db = ConnectionManager::getConnectionInstance();
+    public function enlistUom(UnitOfMeasure $uom) {
         try {
-            $db->beginTransaction();
-            $dbst = $db->prepare('INSERT INTO uom(uom_symbol, uom_desc) VALUES(:symbol, :desc)');
-            $dbst->execute(array('symbol' => $uom->symbol, 'desc' => $uom->description));
-            $db->commit();
+            $this->db->beginTransaction();
+            $dbst = $this->db->prepare('INSERT INTO uom(uom_symbol, uom_desc) VALUES(:symbol, :desc)');
+
+            $params = array('symbol' => $uom->symbol, 'desc' => $uom->description);
+            $dbst->execute($params);
+            ApplicationLoggerUtils::logSql($this->logger, $dbst, $params);
+
+            $this->db->commit();
         } catch (\PDOException $ex) {
-            $db->rollBack();
-            throw new DataAccessException($ex->getMessage());
+            $this->db->rollBack();
+            throw new DataAccessException($ex->getMessage(), $ex);
         }
     }
 
-    public function updateUom($uom) {
-        $db = ConnectionManager::getConnectionInstance();
+    public function updateUom(UnitOfMeasure $uom) {
         try {
-            $db->beginTransaction();
-            $dbst = $db->prepare('UPDATE uom SET uom_symbol=:symbol, uom_desc=:desc WHERE uom_id=:id');
-            $dbst->execute(array('symbol' => $uom->symbol, 'desc' => $uom->description, 'id' => $uom->id));
-            $db->commit();
+            $this->db->beginTransaction();
+            $dbst = $this->db->prepare('UPDATE uom SET uom_symbol=:symbol, uom_desc=:desc WHERE uom_id=:id');
+
+            $params = array('symbol' => $uom->symbol, 'desc' => $uom->description, 'id' => $uom->id);
+            $dbst->execute($params);
+            ApplicationLoggerUtils::logSql($this->logger, $dbst, $params);
+
+            $this->db->commit();
         } catch (\PDOException $ex) {
-            $db->rollBack();
-            throw new DataAccessException($ex->getMessage());
+            $this->db->rollBack();
+            throw new DataAccessException($ex->getMessage(), $ex);
         }
     }
 
     public function getUomInfo($id) {
-        $db = ConnectionManager::getConnectionInstance();
         try {
-            $dbst = $db->prepare('SELECT uom_id, uom_symbol, uom_desc FROM uom WHERE uom_id=:id');
-            $dbst->execute(array('id' => $id));
-
+            $dbst = $this->db->prepare('SELECT uom_id, uom_symbol, uom_desc FROM uom WHERE uom_id=:id');
+            $params = array('id' => $id);
+            $dbst->execute($params);
+            ApplicationLoggerUtils::logSql($this->logger, $dbst, $params);
+            
             $uom = new UnitOfMeasure();
 
             while ($data = $dbst->fetch()) {
@@ -71,7 +87,7 @@ class UnitOfMeasureDaoSqlImpl implements UnitOfMeasureDao {
             }
             return $uom;
         } catch (\PDOException $ex) {
-            throw new DataAccessException($ex->getMessage());
+            throw new DataAccessException($ex->getMessage(), $ex);
         }
     }
 
